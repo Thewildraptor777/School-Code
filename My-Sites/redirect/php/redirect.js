c9abbc0c724712814068a8301cfb391562317a0e
tylerherron777 <>
2023-04-26 21:11:08 +0000

    minor

--- My-Sites/redirect/php/redirect.js
+++ My-Sites/redirect/php/redirect.js
@@ -5,8 +5,11 @@ a = document.getElementById("link");
 if (short == 4000) {
     window.location.replace("My-Sites/Homepage/index.html");
 
-}else if(short==8080){
+}else if(short==4030){
     window.location.replace("Dev/php/write.php")
+}
+else if(short==4040){
+    window.location.replace("ajax/index.html")
 }
 else {
     document.getElementById("manual").classList.remove("hide");
