2caabf6a676b6b6767551d41c4ef865d9c57cc4c
Tyler Herron <tylerherron7777@gmail.com>
2023-04-26 19:43:48 -0500

    branches

--- My-Sites/php/www/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($pass == "Tyler") {
-    echo "<a href='../../index.html'>You many continue</a>";
-    echo "<script src='refresh.js'></script>";
-} else {
-    include "HTML/false.html";
-}
--- My-Sites/redirect/pass.js
+++
@@ -1,4 +0,0 @@
-if (localStorage.getItem("password") == "Tyler") {
-    text2.innerHTML = "You got it!!!";
-    window.location.replace("../Homepage/index.html")
-}
