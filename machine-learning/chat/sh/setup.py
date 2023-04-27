a62a461d120e7be8fd396e66c5060d5a5ca09e00
Tyler Herron <tylerherron7777@gmail.com>
2023-04-26 19:36:47 -0500

    python

--- machine-learning/chat/sh/setup.py
+++ machine-learning/chat/sh/setup.py
@@ -1,7 +1,12 @@
 import subprocess
+subdir = "/workspace/python/machine-learning/chat/sh/"
+
+
 def shFile(pathname):
-    sh_file_path = pathname
+    sh_file_path = subdir+pathname
 
     # Execute the .sh file
     subprocess.call(["/bin/bash", sh_file_path])
-shFile("start/start.sh")
+
+
+shFile("setup.sh")
