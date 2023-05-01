f7c7eb4d6a40f2089abf8ae3d04cea578c72bada
tylerherron777 <>
2023-05-01 21:13:13 +0000

    fileing

--- file.cc
+++ file.cc
@@ -1,40 +1,41 @@
-#include <algorithm>
-#include <list>
-//
-#include <fstream>
+#include <cstring>
 #include <iostream>
-// C++ program to create a directory in Linux
-#include <bits/stdc++.h>
+#include <fstream>
+#include <string>
+#include <list>
 #include <sys/stat.h>
 #include <sys/types.h>
+
 using namespace std;
+
+const string DATA_DIR = "data/";
 
 void createDataDirectory() {
   // Path to the directory
-  const char *dir = "/data";
+  const char *dir = DATA_DIR.c_str();
 
   // Structure which would store the metadata
   struct stat sb;
 
-  // Calls the function with path as argument
+  // Calls the selectLineFromFilection with path as argument
   // If the file/directory exists at the path returns 0
   // If block executes if path exists
   if (stat(dir, &sb) == 0) {
   } else {
 
     // Creating a directory
-    if (mkdir("data", 0777) == -1) {
-      cerr << "Error :  " << strerror(errno) << endl;
+    if (mkdir(DATA_DIR.c_str(), 0777) == -1) {
+      cerr << "Error: " << strerror(errno) << endl;
     } else {
-      cout << "data/ created\n";
+      cout << DATA_DIR << " created\n";
     }
   }
 }
-void createDataFile(string pathname, string data) {
-  std::fstream fileStream;
 
+void createDataFile(string pathname, string data) {
   createDataDirectory();
-  pathname = "data/" + pathname;
+  pathname = DATA_DIR + pathname;
+
   // Create and open a text file
   ofstream MyFile(pathname);
 
@@ -44,39 +45,82 @@ void createDataFile(string pathname, string data) {
   // Close the file
   MyFile.close();
 }
+
 string readDataFileAsWhole(string pathname) {
-    pathname="data/"+pathname;
+  pathname = DATA_DIR + pathname;
   string myText;
-  string finalText="";
+  string finalText = "";
   // Read from the text file
   ifstream MyReadFile(pathname);
 
-  // Use a while loop together with the getline() function to read the file line
-  // by line
+  // Use a while loop together with the getline() selectLineFromFilection to
+  // read the file line by line
   while (getline(MyReadFile, myText)) {
     // Output the text from the file
-    finalText=finalText+myText;
+    finalText = finalText + myText;
   }
 
   // Close the file
   MyReadFile.close();
-return finalText;
+  return finalText;
 }
-string readDataFileAsList(string pathname) {
-    pathname="data/"+pathname;
-  string myText;
-  list<string>finalText={};
-  // Read from the text file
-  ifstream MyReadFile(pathname);
 
-  // Use a while loop together with the getline() function to read the file line
-  // by line
-  while (getline(MyReadFile, myText)) {
-    // Output the text from the file
-    finalText.push_back(myText);
+list<string> readDataFileAsList(string pathname) {
+  // Append the data directory to the pathname
+  pathname = DATA_DIR + pathname;
+
+  // Open the file
+  ifstream inputFile(pathname);
+
+  if (!inputFile.is_open()) {
+    throw runtime_error("Error opening file.");
+  }
+
+  // Read the file line by line and store each line in a list
+  list<string> fileContents;
+  string line;
+  while (getline(inputFile, line)) {
+    fileContents.push_back(line);
   }
 
   // Close the file
-  MyReadFile.close();
-return finalText;
+  inputFile.close();
+
+  return fileContents;
+}
+
+void logLinesOfFile(string pathname) {
+  list<string> myReturnedList = readDataFileAsList(pathname);
+  for (const auto& line : myReturnedList) {
+    cout << line << endl;
+  }
+}
+
+string selectLineFromFile(string filename, int lineNum) {
+  list<string> fileContents = readDataFileAsList(filename);
+
+  // Check if the specified object number is out of range
+  if (lineNum < 1 || lineNum > fileContents.size()) {
+    throw runtime_error("Object number out of range.");
+  }
+
+  // Get the specified line from the list
+  auto it = next(fileContents.begin(), lineNum - 1);
+  string line = *it;
+
+  return line;
+}
+
+void appendToFile(const string& pathname, const string& data) {
+  createDataDirectory();
+  string fullPath = DATA_DIR + pathname;
+
+  // Open the file in append mode
+  ofstream file(fullPath, ios_base::app);
+
+  // Write the data to the file
+  file << endl << data;
+
+  // Close the file
+  file.close();
 }
--- main.cc
+++ main.cc
@@ -1,17 +1,47 @@
 #include <iostream>
 #include "file.cc"
 using namespace std;
+int main() {
+  // Test the createDataDirectory function
+  createDataDirectory();
 
-void runFunction(){
-    cout << "type name\n";
-    string name;
-    cin>> name;
-    cout<< "hi "+name+"\n";
-}
-int main() 
-{    cout<<"\nstarted\n\n";
+  // Test the createDataFile function
+  string pathname = "test.txt";
+  string data = "This is a test file.";
+  createDataFile(pathname, data);
 
-    cout<<readDataFileAsWhole("text.txt");
-    
-    return 0;
+  // Test the readDataFileAsWhole function
+  string fileContents = readDataFileAsWhole(pathname);
+  cout << "File contents: " << fileContents << endl;
+
+  // Test the readDataFileAsList function
+  list<string> fileLines = readDataFileAsList(pathname);
+  cout << "File lines:" << endl;
+  for (const auto& line : fileLines) {
+    cout << line << endl;
+  }
+
+  // Test the logLinesOfFile function
+  logLinesOfFile(pathname);
+
+  // Test the selectLineFromFile function
+  string selectedLine = selectLineFromFile(pathname, 1);
+  cout << "Selected line: " << selectedLine << endl;
+
+  // Test the appendToFile function
+  string newData = "This is some new data.";
+  appendToFile(pathname, newData);
+
+  // Test the readDataFileAsWhole function again to see if the new data was appended
+  fileContents = readDataFileAsWhole(pathname);
+  cout << "File contents after append: " << fileContents << endl;
+
+  // Test the readDataFileAsList function again to see if the new data was appended
+  fileLines = readDataFileAsList(pathname);
+  cout << "File lines after append:" << endl;
+  for (const auto& line : fileLines) {
+    cout << line << endl;
+  }
+
+  return 0;
 }
