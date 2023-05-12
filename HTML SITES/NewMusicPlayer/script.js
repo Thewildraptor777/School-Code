f58916f596d1b548f3651e384a812b18fad1c1dc
Tyler Herron (10) <t.herron326@forneyisd.net>
2023-05-12 16:52:30 +0000

    new mui\sic site

---
+++ HTML SITES/NewMusicPlayer/script.js
@@ -0,0 +1,29 @@
+const tracksList = document.getElementById("playlist-tracks");
+const tracks = tracksList.getElementsByTagName("li");
+const total = document.getElementById("total-songs");
+const playButtonDiv = document.getElementById("play-button-div"); const pauseButtonDiv = document.getElementById("pause-button-div");
+const playButton = document.getElementById("play-button"); const pauseButton = document.getElementById("pause-button");
+
+let totaltracks = tracks.length;
+
+total.innerHTML = total.innerHTML + " " + totaltracks
+playButton.addEventListener("click", () => {
+    pauseButtonDiv.classList.remove("clear")
+    playButtonDiv.classList.add("clear")
+
+});
+pauseButton.addEventListener("click", () => {
+    playButtonDiv.classList.remove("clear")
+    pauseButtonDiv.classList.add("clear")
+
+});
+const shuffleIcon = document.getElementById("shuffle-icon");
+const shuffleButton=document.getElementById("shuffle-button");
+shuffleButton.addEventListener("click", () => {
+    if (shuffleIcon.style.color != "red") 
+    { shuffleIcon.style.color = "red" } 
+    else 
+    { shuffleIcon.style.color = 'black' }
+
+
+})
